561d6e9d541b1a1dbfabbc3e423057a673ac0485
ramverma <ram.krishna.verma@ericsson.com>
2018-08-08 23:17:37 +0100

    Adding configuration parameters for distribution
    Adding main configuration parameters to distribution component. The
    intention is to define a top level parameter group that should be
    capable of handling all the parameters needed for the framework.
    
    Change-Id: I87c4c34d9df2c9b5a0ab75bef2bbe0d25e38747b
    Issue-ID: POLICY-1035
    Signed-off-by: ramverma <ram.krishna.verma@ericsson.com>

---
+++ main/src/main/java/org/onap/policy/distribution/main/parameters/PolicyDecoderParameters.java
@@ -0,0 +1,86 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.distribution.main.parameters;
+
+import org.onap.policy.common.parameters.GroupValidationResult;
+import org.onap.policy.common.parameters.ParameterGroup;
+import org.onap.policy.common.parameters.ValidationStatus;
+
+/**
+ * Class to hold all the policy decoder parameters.
+ *
+ * @author Ram Krishna Verma (ram.krishna.verma@ericsson.com)
+ */
+public class PolicyDecoderParameters implements ParameterGroup {
+    private String decoderType;
+    private String decoderClassName;
+
+    /**
+     * Constructor for instantiating PolicyDecoderParameters.
+     *
+     * @param decoderType the policy decoder type
+     * @param decoderClassName the policy decoder class name
+     */
+    public PolicyDecoderParameters(final String decoderType, final String decoderClassName) {
+        this.decoderType = decoderType;
+        this.decoderClassName = decoderClassName;
+    }
+
+    /**
+     * Return the decoderType of this PolicyDecoderParameters instance.
+     *
+     * @return the decoderType
+     */
+    public String getDecoderType() {
+        return decoderType;
+    }
+
+    /**
+     * Return the decoderClassName of this PolicyDecoderParameters instance.
+     *
+     * @return the decoderClassName
+     */
+    public String getDecoderClassName() {
+        return decoderClassName;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    /**
+     * Validate the policy decoder parameters.
+     *
+     */
+    @Override
+    public GroupValidationResult validate() {
+        final GroupValidationResult validationResult = new GroupValidationResult(this);
+        if (decoderType == null || decoderType.trim().length() == 0) {
+            validationResult.setResult("decoderType", ValidationStatus.INVALID, "must be a non-blank string");
+        }
+        if (decoderClassName == null || decoderClassName.trim().length() == 0) {
+            validationResult.setResult("decoderClassName", ValidationStatus.INVALID,
+                    "must be a non-blank string containing full class name of the decoder");
+        }
+        return validationResult;
+    }
+}
