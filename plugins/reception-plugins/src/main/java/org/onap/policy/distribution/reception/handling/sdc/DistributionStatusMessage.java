f11ca0b74b719bbec2ae4a7b94923f2cb231fed0
ramverma <ram.krishna.verma@ericsson.com>
2018-09-07 21:15:51 +0100

    Adding code for sending distribution status to sdc
    Change-Id: I27c9ed373015d728f1ed02528e2ba2a638952bdf
    Issue-ID: POLICY-956
    Signed-off-by: ramverma <ram.krishna.verma@ericsson.com>

---
+++ plugins/reception-plugins/src/main/java/org/onap/policy/distribution/reception/handling/sdc/DistributionStatusMessage.java
@@ -0,0 +1,88 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.distribution.reception.handling.sdc;
+
+
+import org.onap.sdc.api.consumer.IDistributionStatusMessage;
+import org.onap.sdc.utils.DistributionStatusEnum;
+
+/**
+ * This class represents the distribution status of the distribution service.
+ *
+ * @author Ram Krishna Verma (ram.krishna.verma@ericsson.com)
+ */
+public class DistributionStatusMessage implements IDistributionStatusMessage {
+
+    private String artifactUrl;
+    private String consumerId;
+    private String distributionId;
+    private DistributionStatusEnum distributionStatus;
+    private long timestamp;
+
+    /**
+     * Constructor for instantiating {@link DistributionStatusMessage} class.
+     *
+     * @param artifactUrl the artifact url
+     * @param consumerId the consumer id
+     * @param distributionId the distribution id
+     * @param distributionStatusEnum the distribution status
+     * @param timestamp the status time
+     */
+    public DistributionStatusMessage(final String artifactUrl, final String consumerId, final String distributionId,
+            final DistributionStatusEnum distributionStatusEnum, final long timestamp) {
+        this.artifactUrl = artifactUrl;
+        this.consumerId = consumerId;
+        this.distributionId = distributionId;
+        this.distributionStatus = distributionStatusEnum;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String getArtifactURL() {
+
+        return artifactUrl;
+    }
+
+    @Override
+    public String getConsumerID() {
+
+        return consumerId;
+    }
+
+    @Override
+    public String getDistributionID() {
+
+        return distributionId;
+    }
+
+    @Override
+    public DistributionStatusEnum getStatus() {
+
+        return distributionStatus;
+    }
+
+    @Override
+    public long getTimestamp() {
+
+        return timestamp;
+    }
+
+}
