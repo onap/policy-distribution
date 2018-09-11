353dff1cfa8f98a03abc7279d1de7385c29293c8
liboNet <libo.zhu@intel.com>
2018-09-11 11:07:04 +0800

    Adding code for dynamically generate Polices
    * Update sdc tosca version to 1.4.1
    * Define the objects to represents the policy
      used by OOF/Mutlicloud/AAI etc
    * Integration into the distribution framework
      to update PdpxPolicy and add new File
      PolicyDecoderCsarPdpx to do decoder from CSAR
    * Add two Test case including 2 VNFs, each VNF
      maps to 1 policy, so to generate 2 polices
    
    Change-Id: Icc7175082063db8a245bbe576085cdf5a4b51adc
    Issue-ID: POLICY-927
    Signed-off-by: liboNet <libo.zhu@intel.com>

---
+++ plugins/reception-plugins/src/main/java/org/onap/policy/distribution/reception/decoding/pdpx/FlavorProperty.java
@@ -0,0 +1,84 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Intel. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.distribution.reception.decoding.pdpx;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * The FlavorProperty includes all the properties of Flavor
+ */
+class FlavorProperty{
+    private String hpa_feature;
+    private String mandatory = "true";
+    private String architecture = "generic";
+    private String hpa_version = "v1";
+    private List<Directive> directives = new ArrayList<>();
+    private List<HpaFeatureAttribute> hpa_feature_attributes = new ArrayList<>();
+
+    public void setHpa_feature(String hpa_feature) {
+        this.hpa_feature = hpa_feature;
+    }
+
+    public String getHpa_feature() {
+        return hpa_feature;
+    }
+
+    public void setMandatory(String mandatory) {
+        this.mandatory = mandatory;
+    }
+
+    public String getMandatory() {
+        return mandatory;
+    }
+
+    public void setArchitecture(String architecture) {
+        this.architecture = architecture;
+    }
+
+    public String getArchitecture() {
+        return architecture;
+    }
+
+    public void setHpa_version(String hpa_version) {
+        this.hpa_version = hpa_version;
+    }
+
+    public String getHpa_version() {
+        return hpa_version;
+    }
+
+    public List<Directive> getDirectives() {
+        return directives;
+    }
+
+    public List<HpaFeatureAttribute> getHpa_feature_attributes() {
+        return hpa_feature_attributes;
+    }
+
+    @Override
+    public String toString() {
+        return "{ hpa_feature:" + hpa_feature + ", mandatory = " + mandatory + ",architecture:" + architecture
+            + ", hpa_feature_attributes : [" + hpa_feature_attributes + "]";
+    }
+
+}
+
---
+++ plugins/reception-plugins/src/main/java/org/onap/policy/distribution/reception/decoding/pdpx/PolicyDecoderCsarPdpx.java
@@ -0,0 +1,96 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.distribution.reception.decoding.pdpx;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+import org.onap.policy.common.logging.flexlogger.FlexLogger;
+import org.onap.policy.common.logging.flexlogger.Logger;
+
+import org.onap.policy.distribution.model.PolicyInput;
+import org.onap.policy.distribution.model.Csar;
+import org.onap.policy.distribution.reception.decoding.PolicyDecoder;
+import org.onap.policy.distribution.reception.decoding.PolicyDecodingException;
+
+import org.onap.sdc.tosca.parser.api.ISdcCsarHelper;
+import org.onap.sdc.tosca.parser.impl.SdcCsarHelperImpl;
+import org.onap.sdc.tosca.parser.impl.SdcToscaParserFactory;
+
+import org.onap.sdc.toscaparser.api.NodeTemplate;
+import org.onap.sdc.toscaparser.api.elements.Metadata;
+
+/**
+ * Decodes PDP-X policies from a TOSCA file.
+ */
+public class PolicyDecoderCsarPdpx implements PolicyDecoder<Csar, PdpxPolicy> {
+
+    private static final Logger LOGGER = FlexLogger.getLogger(PolicyDecoderCsarPdpx.class);
+
+    ISdcCsarHelper sdcCsarHelper;
+
+
+    @Override
+    public Collection<PdpxPolicy> decode(Csar csar) throws PolicyDecodingException {
+        // logic for generating the policies from the CSAR. 
+        List<PdpxPolicy> lPdpxPolicy = new ArrayList<>();
+        parseCsar(csar);
+        List<NodeTemplate> lnodeVf = sdcCsarHelper.getServiceVfList();
+        LOGGER.debug("the size of Vf = " + lnodeVf.size());
+        ExtractFromNode extractFromNode = new ExtractFromNode();
+        extractFromNode.setSdcCsarHelper(sdcCsarHelper);
+        for ( NodeTemplate node : lnodeVf){
+                PdpxPolicy ret = extractFromNode.extractInfo(node);
+                if(ret != null) {
+                    lPdpxPolicy.add(ret);
+                }
+        }
+        return lPdpxPolicy;
+    }
+
+    @Override
+    public boolean canHandle(PolicyInput policyInput) {
+        return policyInput.getClass().isAssignableFrom(Csar.class);
+    }
+
+    public void parseCsar(Csar csar)  throws PolicyDecodingException {
+        try {
+
+            SdcToscaParserFactory factory = SdcToscaParserFactory.getInstance();//Autoclosable
+
+            LOGGER.debug("tosca File Path = " + csar.getCsarPath());
+
+            File spoolFile = new File(csar.getCsarPath());
+
+            sdcCsarHelper = factory.getSdcCsarHelper(spoolFile.getAbsolutePath());
+
+        }catch(Exception e){
+            LOGGER.error("Exception got in parseTosca",e);
+            throw new PolicyDecodingException ("Exception caught when passing the csar file to the parser ", e);
+        }
+    }
+
+}
