9efe81e6cd3bb45091ab25d4b803f24197bcb361
ramverma <ram.krishna.verma@ericsson.com>
2018-09-18 20:15:02 +0100

    Fix sonar & add test cases for models
    * As per jacoco, the try-with-resources coverage issue is fixed in 0.8.0
    release - https://www.jacoco.org/jacoco/trunk/doc/changes.html
    
    * Adding openpojo test for all the models in distribution-model
    
    Change-Id: I55e2891a0dad1f10113730b46f10d760a883bea6
    Issue-ID: POLICY-922
    Signed-off-by: ramverma <ram.krishna.verma@ericsson.com>

---
+++ model/src/test/java/org/onap/policy/distribution/model/TestModels.java
@@ -0,0 +1,43 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.distribution.model;
+
+import com.openpojo.reflection.filters.FilterPackageInfo;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+
+import org.junit.Test;
+
+/**
+ * Class to perform unit testing of all policy models.
+ *
+ * @author Ram Krishna Verma (ram.krishna.verma@ericsson.com)
+ */
+public class TestModels {
+
+    @Test
+    public void testAllModels() {
+        final Validator validator = ValidatorBuilder.create().with(new SetterTester()).with(new GetterTester()).build();
+        validator.validate(Policy.class.getPackage().getName(), new FilterPackageInfo());
+    }
+}
