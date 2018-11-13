72fa3c3d99cd6db705e4a8ca35c79ece9f0985c2
Pamela Dragosh <pdragosh@research.att.com>
2018-11-13 10:19:07 -0500

    Fix unchecked cast sonar issue
    The variable is currently unused as flagged by sonar. But
    the real issue is the @SuppressWarnings("unchecked") should
    not be used unless there is a check to ensure that the
    object you are casting to really is that object.
    
    So added the check for that which should remove the
    original sonar issues.
    
    Issue-ID: POLICY-1256
    Change-Id: Iad7dbbf02a4dd48648fc9d5c20595c9f0f8d7acb
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- plugins/reception-plugins/src/main/java/org/onap/policy/distribution/reception/handling/sdc/FileSystemReceptionHandler.java
+++ plugins/reception-plugins/src/main/java/org/onap/policy/distribution/reception/handling/sdc/FileSystemReceptionHandler.java
@@ -75,7 +75,6 @@ public class FileSystemReceptionHandler extends AbstractReceptionHandler {
      * 
      * @param watchPath Path to watch
      */
-    @SuppressWarnings("unchecked")
     public void main(String watchPath) throws IOException {
         try (final WatchService watcher = FileSystems.getDefault().newWatchService()) {
             final Path dir = Paths.get(watchPath);
@@ -96,7 +95,10 @@ public class FileSystemReceptionHandler extends AbstractReceptionHandler {
             key = watcher.take();
 
             for (final WatchEvent<?> event : key.pollEvents()) {
-                final WatchEvent.Kind<?> kind = event.kind();
+                if ( ! (event.kind() instanceof Path) ) {
+                    LOGGER.debug("Unsupported watch event object " + event.kind().getClass().getCanonicalName());
+                    continue;
+                }
                 final WatchEvent<Path> ev = (WatchEvent<Path>) event;
                 final Path fileName = ev.context();
                 LOGGER.debug("new CSAR found: " + fileName);
