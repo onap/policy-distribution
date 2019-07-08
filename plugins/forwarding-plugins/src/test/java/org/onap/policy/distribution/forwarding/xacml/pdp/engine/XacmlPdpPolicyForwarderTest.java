c424ad1cdde04b21c6d4fb22a58c2cbbf23bc2ed
Jim Hahn <jrh3@att.com>
2019-07-08 12:07:42 -0400

    Fix distribution due to sonar changes in common
    Fixed breakages due to changes made in policy/common to satisfy
    sonar.
    
    Change-Id: Ib1e57668f727db1b87a9aa3bc9a8266884f5e994
    Issue-ID: POLICY-1791
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- plugins/forwarding-plugins/src/test/java/org/onap/policy/distribution/forwarding/xacml/pdp/engine/XacmlPdpPolicyForwarderTest.java
+++ plugins/forwarding-plugins/src/test/java/org/onap/policy/distribution/forwarding/xacml/pdp/engine/XacmlPdpPolicyForwarderTest.java
@@ -1,19 +1,20 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -31,7 +32,6 @@ import static org.mockito.Mockito.when;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -50,7 +50,6 @@ import org.onap.policy.common.parameters.ParameterGroup;
 import org.onap.policy.common.parameters.ParameterService;
 import org.onap.policy.distribution.forwarding.xacml.pdp.XacmlPdpPolicyForwarder;
 import org.onap.policy.distribution.forwarding.xacml.pdp.XacmlPdpPolicyForwarderParameterGroup.XacmlPdpPolicyForwarderParameterGroupBuilder;
-import org.onap.policy.distribution.main.PolicyDistributionException;
 import org.onap.policy.distribution.model.OptimizationPolicy;
 import org.onap.policy.distribution.model.Policy;
 
@@ -79,9 +78,7 @@ public class XacmlPdpPolicyForwarderTest {
     }
 
     @Test
-    public void testForwardPolicy()
-            throws KeyManagementException, NoSuchAlgorithmException, NoSuchFieldException,SecurityException,
-            IllegalArgumentException, IllegalAccessException, PolicyDistributionException, ClassNotFoundException {
+    public void testForwardPolicy() throws Exception {
 
         HttpClient httpClientMock = mock(HttpClient.class);
         headers.put(CLIENT_AUTH, CLIENT_AUTH_VALUE);
@@ -122,9 +119,7 @@ public class XacmlPdpPolicyForwarderTest {
     }
 
     @Test
-    public void testForwardPolicy_CreateFailsPushNotInvoked()
-            throws KeyManagementException, NoSuchAlgorithmException, NoSuchFieldException, SecurityException,
-            IllegalArgumentException, IllegalAccessException, PolicyDistributionException, ClassNotFoundException {
+    public void testForwardPolicy_CreateFailsPushNotInvoked() throws Exception {
 
         HttpClient httpClientMock = mock(HttpClient.class);
         headers.put(CLIENT_AUTH, CLIENT_AUTH_VALUE);
@@ -150,9 +145,7 @@ public class XacmlPdpPolicyForwarderTest {
     }
 
     @Test
-    public void testForwardPolicy_PushFails()
-            throws KeyManagementException, NoSuchAlgorithmException, NoSuchFieldException, SecurityException,
-            IllegalArgumentException, IllegalAccessException, PolicyDistributionException, ClassNotFoundException {
+    public void testForwardPolicy_PushFails() throws Exception {
 
         HttpClient httpClientMock = mock(HttpClient.class);
         headers.put(CLIENT_AUTH, CLIENT_AUTH_VALUE);
@@ -179,9 +172,7 @@ public class XacmlPdpPolicyForwarderTest {
     }
 
     @Test
-    public void testForwardPolicy_HttpClientInitFailureForPolicyCreate()
-            throws KeyManagementException, NoSuchAlgorithmException, NoSuchFieldException, SecurityException,
-            IllegalArgumentException, IllegalAccessException, PolicyDistributionException, ClassNotFoundException {
+    public void testForwardPolicy_HttpClientInitFailureForPolicyCreate() throws Exception {
 
         HttpClient httpClientMock = mock(HttpClient.class);
         headers.put(CLIENT_AUTH, CLIENT_AUTH_VALUE);
@@ -207,9 +198,7 @@ public class XacmlPdpPolicyForwarderTest {
     }
 
     @Test
-    public void testForwardPolicy_HttpClientInitFailureForPolicyPush()
-            throws KeyManagementException, NoSuchAlgorithmException, NoSuchFieldException, SecurityException,
-            IllegalArgumentException, IllegalAccessException, PolicyDistributionException, ClassNotFoundException {
+    public void testForwardPolicy_HttpClientInitFailureForPolicyPush() throws Exception {
 
         HttpClient httpClientMock = mock(HttpClient.class);
         headers.put(CLIENT_AUTH, CLIENT_AUTH_VALUE);
