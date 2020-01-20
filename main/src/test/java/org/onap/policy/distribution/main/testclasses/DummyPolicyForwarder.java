7d62a13f6fe6fb9428fd66ba9d36326c42149830
HOCKLA <ah999m@att.com>
2020-01-20 16:37:27 -0600

    policy/distribution jdk11 upgrades
    Issue-ID: POLICY-1585
    Change-Id: I4aa879656c039d9de9c288f0309105c21f4bf285
    Signed-off-by: HOCKLA <ah999m@att.com>

--- main/src/test/java/org/onap/policy/distribution/main/testclasses/DummyPolicyForwarder.java
+++ main/src/test/java/org/onap/policy/distribution/main/testclasses/DummyPolicyForwarder.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,5 +52,6 @@ public class DummyPolicyForwarder implements PolicyForwarder {
     }
 
     @Override
-    public void configure(final String parameterGroupName) {}
+    public void configure(final String parameterGroupName) {
+    }
 }
--- plugins/reception-plugins/src/test/java/org/onap/policy/distribution/reception/handling/sdc/DummyDecoder.java
+++ plugins/reception-plugins/src/test/java/org/onap/policy/distribution/reception/handling/sdc/DummyDecoder.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2018 Ericsson. All rights reserved.
+ *  Modifications Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,5 +66,6 @@ public class DummyDecoder implements PolicyDecoder<Csar, DummyPolicy> {
     }
 
     @Override
-    public void configure(final String parameterGroupName) {}
+    public void configure(final String parameterGroupName) {
+    }
 }
