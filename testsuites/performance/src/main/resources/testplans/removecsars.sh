7bf29a29694a940824cb0c0fc205204de6495900
RossC <ross.carter@est.tech>
2020-05-14 10:40:00 +0100

    Frankfurt Distribution Performance Test
    Issue-ID: POLICY-2458
    Change-Id: I475ac795256db980ae68993a8cbc509d74278549
    Signed-off-by: RossC <ross.carter@est.tech>

---
+++ testsuites/performance/src/main/resources/testplans/removecsars.sh
@@ -0,0 +1,40 @@
+# ============LICENSE_START=======================================================
+#  Copyright (c) 2020 Nordix Foundation.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
+
+# the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "${DIR}"
+
+if [  $# -le 0 ]
+then
+  echo "Usage ./removecsars.sh \$TARGETDIRECTORY"
+  exit 1
+fi
+TARGETDIRECTORY=$1
+
+for file in "$TARGETDIRECTORY"/*
+do
+  if [[ $file == *.csar ]]
+  then
+      rm -rf "$file"
+  fi
+done
+
+
+
