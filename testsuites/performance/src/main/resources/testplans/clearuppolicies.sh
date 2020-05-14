5f3c6aba1ef89f41e82b8d47bef8731b679da971
RossC <ross.carter@est.tech>
2020-05-14 10:40:00 +0100

    Frankfurt Distribution Performance Test
    Issue-ID: POLICY-2458
    Change-Id: I475ac795256db980ae68993a8cbc509d74278549
    Signed-off-by: RossC <ross.carter@est.tech>

---
+++ testsuites/performance/src/main/resources/testplans/clearuppolicies.sh
@@ -0,0 +1,43 @@
+# ============LICENSE_START=======================================================
+#  Copyright (c) 2020 Nordix Foundation.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
+
+# the directory of the script
+if [  $# -le 3 ]
+  then
+    echo "Usage ./addcsars.sh \$POLICY_API_IP \$POLICY_API_PORT \$POLICY_PAP_IP \$POLICY_PAP_PORT"
+    exit 1
+fi
+POLICY_API_IP=$1
+POLICY_API_PORT=$2
+POLICY_PAP_IP=$3
+POLICY_PAP_PORT=$4
+
+send_delete_request(){
+  echo $1
+  curl -k -u 'healthcheck:zb!XztG34' -X DELETE "$1"
+  COUNTER=$((COUNTER +1))
+}
+
+COUNTER=1
+while [ $COUNTER != 11 ]
+do
+  send_delete_request https://"$POLICY_PAP_IP":"$POLICY_PAP_PORT"/policy/pap/v1/pdps/policies/operational.apex.\
+sampledomain.test$COUNTER
+  send_delete_request https://"$POLICY_API_IP":"$POLICY_API_PORT"/policy/api/v1/policytypes/operational.apex.\
+sampledomain.test$COUNTER/versions/1.0.0/policies/operational.apex.sampledomain.test$COUNTER/versions/1.0.0
+done
