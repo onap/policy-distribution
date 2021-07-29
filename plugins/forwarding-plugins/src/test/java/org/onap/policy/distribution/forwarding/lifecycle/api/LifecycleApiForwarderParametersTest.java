44d858c1534487b5182b4813164a544f72055088
Jim Hahn <jrh3@att.com>
2021-07-29 16:35:49 -0400

    Use new RestClientParameters class in distribution
    Replaced LifecycleApiForwarderParameters with RestClientParameters.
    
    This requires changes to property files in CSITs and OOM:
    - rename "hostName" to "hostname"
    - rename "isHttps" to "useHttps" and move it inside the api & pap
      structures
    
    Also removed some unused test json files.
    
    Issue-ID: POLICY-3456
    Change-Id: I32b04bc3b6880ec7798814dad268143bc3b06faf
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- plugins/forwarding-plugins/src/test/java/org/onap/policy/distribution/forwarding/lifecycle/api/LifecycleApiForwarderParametersTest.java
+++ plugins/forwarding-plugins/src/test/java/org/onap/policy/distribution/forwarding/lifecycle/api/LifecycleApiForwarderParametersTest.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2019 Nordix Foundation.
+ *  Modifications Copyright (C) 2021 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,8 +21,8 @@
 
 package org.onap.policy.distribution.forwarding.lifecycle.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -36,13 +37,6 @@ import org.onap.policy.distribution.forwarding.testclasses.CommonTestData;
 public class LifecycleApiForwarderParametersTest {
 
     private static final String POLICY_API_HOST_NAME = "0.0.0.0";
-    private static final int POLICY_API_PORT = 6969;
-    private static final String POLICY_API_USER = "healthcheck";
-    private static final String POLICY_API_PASSWORD = "zb!XztG34";
-    private static final String POLICY_PAP_HOST_NAME = "0.0.0.0";
-    private static final int POLICY_PAP_PORT = 6969;
-    private static final String POLICY_PAP_USER = "healthcheck";
-    private static final String POLICY_PAP_PASSWORD = "zb!XztG34";
 
 
     @Test
@@ -54,17 +48,13 @@ public class LifecycleApiForwarderParametersTest {
 
         assertEquals(LifecycleApiForwarderParameters.class.getSimpleName(),
                 configurationParameters.getName());
-        assertFalse(configurationParameters.isHttps());
         assertTrue(configurationParameters.isDeployPolicies());
-        assertEquals(POLICY_API_HOST_NAME, configurationParameters.getApiParameters().getHostName());
-        assertEquals(POLICY_API_PORT, configurationParameters.getApiParameters().getPort());
-        assertEquals(POLICY_API_USER, configurationParameters.getApiParameters().getUserName());
-        assertEquals(POLICY_API_PASSWORD, configurationParameters.getApiParameters().getPassword());
-        assertEquals(POLICY_PAP_HOST_NAME, configurationParameters.getPapParameters().getHostName());
-        assertEquals(POLICY_PAP_PORT, configurationParameters.getPapParameters().getPort());
-        assertEquals(POLICY_PAP_USER, configurationParameters.getPapParameters().getUserName());
-        assertEquals(POLICY_PAP_PASSWORD, configurationParameters.getPapParameters().getPassword());
+        assertEquals(POLICY_API_HOST_NAME, configurationParameters.getApiParameters().getHostname());
 
+        configurationParameters.getApiParameters().setClientName("api");
+        configurationParameters.getPapParameters().setClientName("pap");
+
+        assertThat(configurationParameters.validate().getResult()).isNull();
         assertEquals(ValidationStatus.CLEAN, configurationParameters.validate().getStatus());
     }
 
